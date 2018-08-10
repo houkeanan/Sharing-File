bd6e35cc9b28dce250999a9843d661c2c9841577
houkeanan <hou.keanan@gmail.com>
2018-08-10 10:24:43 +0700

    list-fishes.php

---
+++ list-fishes.php
@@ -0,0 +1,19 @@
+<?php 
+	$result = '[{"fish_img":"fish_happy-1.png","fish_name":"Indian Mackerel","cat_name":"Marine Fish","size_name":"Medium","price":"100"},';
+	$result .= '{"fish_img":"fish_happy-1.png","fish_name":"Manthal Repti","cat_name":"Marine Fish","size_name":"Small","price":"200"},';
+	$result .= '{"fish_img":"fish_happy-1.png","fish_name":"Baby Sole Fish","cat_name":"Marine Fish","size_name":"Small","price":"600"},';
+	$result .= '{"fish_img":"fish_happy-1.png","fish_name":"Silver Pomfret","cat_name":"Marine Fish","size_name":"Large","price":"300"},';
+	$result .= '{"fish_img":"fish_happy-1.png","fish_name":"Squid","cat_name":"Shell Fish","size_name":"Small","price":"800"},';
+	$result .= '{"fish_img":"6.jpg","fish_name":"Clam Meat","cat_name":"Shell Fish","size_name":"Small","price":"350"},';
+	$result .= '{"fish_img":"7.jpg","fish_name":"Indian Prawns","cat_name":"Shell Fish","size_name":"Medium","price":"270"},';
+	$result .= '{"fish_img":"8.jpg","fish_name":"Mud Crab","cat_name":"Shell Fish","size_name":"Medium","price":"490"},';
+	$result .= '{"fish_img":"9.jpg","fish_name":"Grey Mullet","cat_name":"Backwater Fish","size_name":"Small","price":"670"},';
+	$result .= '{"fish_img":"10.jpg","fish_name":"Baasa","cat_name":"Backwater Fish","size_name":"Large","price":"230"},';
+	$result .= '{"fish_img":"11.jpg","fish_name":"Pearl Spot","cat_name":"Backwater Fish","size_name":"Small","price":"340"},';
+	$result .= '{"fish_img":"12.jpg","fish_name":"Anchovy","cat_name":"Marine Fish","size_name":"Small","price":"130"},';
+	$result .= '{"fish_img":"13.jpg","fish_name":"Sole Fish","cat_name":"Marine Fish","size_name":"Medium","price":"250"},';
+	$result .= '{"fish_img":"14.jpg","fish_name":"Silver Croaker","cat_name":"Marine Fish","size_name":"Small","price":"220"}]';
+	
+  echo $result;
+
+?>
